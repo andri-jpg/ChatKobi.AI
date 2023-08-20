6e331be735680e6d691cefdc6b09151bf1f3ad01
Andrian Syah putra <andriansyahputra260@gmail.com>
2023-08-20 18:00:53 +0700

    new logo and improved backend

--- app-build/setup_app.py
+++ app-build/setup_app.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name="chatkobi-api",
-    version="0.2",
+    version="0.2.2",
     description="chatkobi_backend",
     executables=[Executable("app.py")],
     options={"build_exe": {"packages": ["uvicorn"]}}
